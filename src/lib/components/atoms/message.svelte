4bf7ac0544dc8ec0212ef5f0ad3f815004274d12
Myphz <danielkeq@gmail.com>
2025-05-24 11:16:38 +0200

    feat: send read notifications

--- src/lib/components/atoms/message.svelte
+++ src/lib/components/atoms/message.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { page } from "$app/state";
   import type { ServerMessage } from "$lib/api/protocol";
+  import { sendMessage } from "$lib/api/ws";
+  import { onMount } from "svelte";
 
   type Message = Extract<
     ServerMessage,
@@ -7,6 +10,43 @@
   >["data"]["messages"][number];
 
   const message: Message = $props();
+
+  let container: HTMLDivElement;
+
+  const onRead = () =>
+    sendMessage({
+      type: "READ_MESSAGE",
+      chatId: page.params.chat,
+      data: {
+        messageId: message.id
+      }
+    });
+
+  onMount(() => {
+    // TODO: Add || message.senderId is us
+    if (message.isRead) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && entry.intersectionRatio === 1) {
+            // If the div is intersecting and fully visible (ratio is 1)
+            onRead();
+            observer.disconnect();
+          }
+        });
+      },
+      {
+        threshold: 1
+      }
+    );
+
+    observer.observe(container);
+
+    return () => {
+      observer.disconnect();
+    };
+  });
 </script>
 
-<div>{message.content}</div>
+<div bind:this={container}>{message.content}</div>
