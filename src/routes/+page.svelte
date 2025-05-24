a607301b53c332f44ed9aa6eb2506e8a90932b73
Myphz <danielkeq@gmail.com>
2025-05-24 12:24:46 +0200

    feat: detect chat name automatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
   import type { ServerMessage } from "$lib/api/protocol";
   import { onMessage } from "$lib/api/ws";
+  import { getJWT } from "$lib/supabase/client";
 
   let chats = $state<Extract<ServerMessage, { type: "INITIAL_SYNC" }>["chats"]>(
     []
   );
 
   onMessage((msg) => {
-    chats = msg.chats;
+    chats = msg.chats.map((chat) => ({
+      ...chat,
+      name:
+        chat.name ||
+        chat.members.find((member) => member.id !== getJWT().id)?.name ||
+        "UNKNOWN"
+    }));
   }, "INITIAL_SYNC");
 </script>
 
