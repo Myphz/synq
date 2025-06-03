7189c5ef456c67ce92a06f8def4b18158ad8b41c
Myphz <danielkeq@gmail.com>
2025-06-03 19:56:03 +0200

    feat: add notifications plugin

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,36 @@ import { App } from "@capacitor/app";
 import { StatusBar, Style } from "@capacitor/status-bar";
 import { Capacitor } from "@capacitor/core";
 import { closeSocket, getSocket_forced } from "$lib/api/ws";
+import { PushNotifications } from "@capacitor/push-notifications";
+import { supabase } from "$lib/supabase/client";
+import { getSupabaseSession, getUserId } from "$lib/supabase/auth/utils";
+
+export const setupNotifications = async () => {
+  let permStatus = await PushNotifications.checkPermissions();
+
+  if (permStatus.receive === "prompt") {
+    permStatus = await PushNotifications.requestPermissions();
+  }
+
+  if (permStatus.receive !== "granted") {
+    throw new Error("User denied permissions!");
+  }
+
+  await PushNotifications.addListener("registration", async (token) => {
+    const fcmToken = token.value;
+    // Save token in supabase
+    if (!(await getSupabaseSession())) return;
+    const userId = await getUserId();
+
+    await supabase
+      .from("profiles")
+      .update({ fcm_token: fcmToken })
+      .eq("id", userId)
+      .throwOnError();
+  });
+
+  await PushNotifications.register();
+};
 
 export const appConfig = () => {
   App.addListener("backButton", () => {
@@ -26,6 +56,8 @@ export const appConfig = () => {
     if (isActive) getSocket_forced();
     else closeSocket();
   });
+
+  setupNotifications();
 
   // App.addListener("pause", closeSocket);
   // App.addListener("resume", getSocket_forced);
