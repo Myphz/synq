776c86d6da9da8eb3fa5e1eba2427ead86e05168
Myphz <danielkeq@gmail.com>
2025-12-07 12:37:58 +0100

    fix: correct max length for chat preview last message

--- src/lib/components/atoms/chat-preview.svelte
+++ src/lib/components/atoms/chat-preview.svelte
@@ -10,20 +10,20 @@
   const chat = $derived(getChat(chatId));
 </script>
 
-<a class="flex w-full items-center justify-between" href="/{chat.chatId}">
-  <div class="flex items-center gap-2">
+<a class="flex w-full items-center justify-between gap-4" href="/{chat.chatId}">
+  <div class="flex min-w-0 flex-1 items-center gap-2">
     <CyberImage src={chat.image} class="size-12" />
-    <div class="flex flex-col justify-center gap-1 leading-none">
+    <div class="flex min-w-0 flex-col justify-center gap-1 leading-none">
       <span class="text-h-4 text-primary">{chat.name}</span>
       {#if chat.lastMessage}
-        <span class="line-clamp-1 text-ellipsis">
+        <span class="overflow-hidden text-ellipsis whitespace-nowrap">
           {chat.lastMessage.content}
         </span>
       {/if}
     </div>
   </div>
 
-  <div class="flex flex-col items-end gap-1.5">
+  <div class="flex shrink-0 flex-col items-end gap-1.5">
     {#if chat.lastMessage}
       <span class="text-small text-muted">
         {#if isToday(chat.lastMessage.sentAt)}
